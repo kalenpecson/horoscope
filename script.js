4b8dee35180930ecec41256fc47365c345ad1468
kalen pecson <kalenpecson@students.berkeley.net>
2017-11-07 11:19:18 -0800

    Final Draft

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function getHoroscope(){
+    document.getElementById("output").style.display="block";
     var day = document.getElementById("days").value;
     var month = document.getElementById("month").value;
     var person = document.getElementById("name").value;
@@ -8,12 +9,31 @@ function getHoroscope(){
     var horoscope = "";
     var horoscopesdates = [0, "Capricorn", 119, "Aquarius", 218, "Pisces", 320, "Aries", 419, "Taurus", 520, "Gemini", 620, "Cancer", 722,
         "Leo", 822, "Virgo", 922, "Libra", 1022, "Scorpio", 1121, "Sagittarius", 1221, "Capricorn", 1231];
+    var horoscopemessages=["Capricorn", "You will get what you want and then die happily", "Aquarius", "Your smile will illuminate the lives of those around you to the point that they will love you forever",
+        "Pisces", "Although it may seem like everything is not going so well right now, life will take a turn for the better when you find your soulmate",
+        "Aries", "What do you do when life gives you lemons? You make some shmacking sorbet! This how you tend live life and it will reward you with much joy and ice cream",
+        "Taurus", "You will one become a waiter at a diner. The day a man asks you for no ketchup but all sauce on their burger, you must quit your job and become a scuba diver in the northeastern part of New Guinea",
+        "Gemini", "You will become addicted to wearing polka dotted pants and end up deciding you prefer an amish lifestyle", "Cancer", "Although your parents may have been strict with you growing up, the essence of your life is based on the fact that you love to eat persimmons",
+    "Leo", "The next time you go to the barber, they will refuse to serve on the basis of lack of cleansiness. This will cause you to go bald and take many bubble baths regularly", "Virgo",
+        "When you were a kid, Santa Claus may have not given you what you desired, which is why you will become a fake santa and truly make kid's wishes come true", "Libra","The day you decide to buy organic will be the day your soul will finally be free",
+        "Scorpio","The difference between you and a professional sports player is the money, not the talent", "Sagittarius", "Altough it may seem like what you do is for money, people do not appreciate the work you put in and the talent you have at such a young age"];
+
+    n =  new Date();
+    m = n.getMonth() + 1;
+    d = n.getDate();
+    if (d== day && m==month){
+        document.body.style.backgroundImage = "url('http://images.all-free-download.com/images/graphiclarge/happy_birthday_elements_02_vector_156400.jpg')";
+    }
+
+
     for (var i = 0; i < horoscopesdates.length; i += 2) {
         if (date > horoscopesdates[i] && date < horoscopesdates[i + 2]) {
             horoscope = horoscopesdates[i + 1];
         }
-        document.getElementById("output").innerHTML = "Dear " + person + ", you are a " + horoscope + "!";
-        document.getElementById("image").innerHTML = "<img src='img/" + horoscope + "_icon.png'/>";
+        var one= horoscopemessages.indexOf(horoscope);
+        var two = horoscopemessages[one+1];
+        document.getElementById("output").innerHTML = "Dear " + person + ", you are a " + horoscope + "!" + "<br/>" +two;
+        document.getElementById("image").innerHTML = "<img src='img/" + horoscope + "_icon.png' width='250'/>";
 
     }
 }
@@ -42,29 +62,4 @@ function monthlength(){
 }
 
 
-// <script language= "javascript">
-//     "text"
-//     </script>
 
-
-
-// Aries (March 21-April 19)
-// Taurus (April 20-May 20)
-//
-// Gemini (May 21-June 20)
-//
-// Cancer (June 21-July 22)
-//
-// Leo (July 23-August 22)
-// Virgo (August 23-September 22)
-//
-// Libra (September 23-October 22)
-//
-// Scorpio (October 23-November 21)
-//
-// Sagittarius (November 22-December 21)
-// Capricorn (December 22-January 19)
-//
-// Aquarius (January 20 to February 18)
-//
-// Pisces (February 19 to March 20)
